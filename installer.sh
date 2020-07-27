3ad99d0ca04d0e96a81719602eb7161252efa6d1
Alijah O'Connor <aoconnor@archerdx.com>
2020-07-27 10:25:07 -0600

    Install via brew OR yum

--- installer.sh
+++ installer.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-install_via_brew() {
+install_via_brew_or_yum() {
     package=$1
 
     # Install bash-completion
     if type brew &> /dev/null; then
         brew install $package
+    elif type yum &> /dev/null; then
+        yum install -y $package
     else
-        echo "Could not download $package on this system because there is no brew present; need to find another way"
+        echo "Could not download $package on this system because there is no brew or yum present; need to find another way"
     fi
 }
 
@@ -18,9 +20,9 @@ main() {
         exit 1
     fi
 
-    install_via_brew bash-completion
-    install_via_brew fzf
-    install_via_brew ripgrep
+    install_via_brew_or_yum bash-completion
+    install_via_brew_or_yum fzf
+    install_via_brew_or_yum ripgrep
 
     echo -e "\nsource ~/.dotfiles/aliases" >> ~/.bash_profile
     echo -e "source ~/.dotfiles/bash_profile\n" >> ~/.bash_profile
